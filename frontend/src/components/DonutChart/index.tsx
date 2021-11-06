91b04d33d6d255d4318e1f5c6390aba676b7369c
Iury Sousa <iurysousa13@outlook.com>
2021-11-06 00:13:05 -0300

    chore: donet chart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,6 @@
 import { ApexOptions } from 'apexcharts';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'src/types/sale';
 import { BASE_URL } from 'src/utils/constants';
@@ -10,33 +11,29 @@ type ChartData = {
 };
 
 export function DonutChart() {
-  let chartData: ChartData = { labels: [], series: [] };
+  const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-  axios.get<SaleSum[]>(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
-    const data = response.data;
-    const labels: string[] = [];
-    const series: number[] = [];
+  useEffect(() => {
+    axios.get<SaleSum[]>(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+      const data = response.data;
+      const labels: string[] = [];
+      const series: number[] = [];
 
-    data.forEach((saleSum) => {
-      labels.push(saleSum.sellerName);
-      series.push(saleSum.sum);
+      data.forEach((saleSum) => {
+        labels.push(saleSum.sellerName);
+        series.push(saleSum.sum);
+      });
+
+      setChartData({ labels, series });
     });
-
-    chartData = { labels, series };
-
-    console.log(chartData);
-  });
-
-  const mockData = {
-    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-    series: [477138, 499928, 444867, 220426, 473086],
-    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé'],
-  };
+  }, []);
 
   const options: ApexOptions = {
     legend: {
       show: true,
     },
   };
-  return <Chart options={{ ...options, labels: mockData.labels }} series={mockData.series} type='donut' height='240' />;
+  return (
+    <Chart options={{ ...options, labels: chartData.labels }} series={chartData.series} type='donut' height='240' />
+  );
 }
