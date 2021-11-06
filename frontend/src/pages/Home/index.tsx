34f1c98e683d64a20df233213f85a32ec936a22b
Iury Sousa <iurysousa13@outlook.com>
2021-11-05 23:30:06 -0300

    chore: home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,25 @@
+import { Footer, NavBar } from '@/components';
+import { Link } from 'react-router-dom';
+
 export function HomePage() {
   return (
-    <div>
-      <h1>Teste</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className='container'>
+        <div className='jumbotron'>
+          <h1 className='display-4'>DSVendas</h1>
+          <p className='lead'>Analise o desempenho das suas vendas por diferentes perspectivas</p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com
+            Spring Boot.
+          </p>
+          <Link className='btn btn-primary btn-lg' to='/dashboard'>
+            Acessar dashboard
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
